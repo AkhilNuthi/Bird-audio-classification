f0607918a112b0a23b0d3dbd04fc6104ab90608e
Akhil0507 <85498044+AkhilNuthi@users.noreply.github.com>
2023-09-08 23:42:44 +0530

    Add files via upload

---
+++ birdwebsite/bird.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+import librosa
+import noisereduce as nr
+import os
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+
+# Load your pre-trained model
+model = tf.keras.models.load_model(r"C:\Users\N Akhil\bird\bird_species_model.h5")  # Replace with your model path
+
+# Define a function for bird species prediction
+def predict_bird_species(audio_data, sample_rate,le):
+    # Extract audio features (e.g., MFCCs or spectrogram) from audio_data
+    audio_features = extract_features(audio_data, sample_rate)
+    audio_features = audio_features.T  # Transpose to match (num_frames, num_mfcc) shape
+    audio_features = np.expand_dims(audio_features, axis=0)
+    prediction = model.predict(audio_features)  # Reshape to match the model's input shape
+    predicted_class_index = np.argmax(prediction)
+    predicted_class = le.inverse_transform([predicted_class_index])[0]
+    return predicted_class
+
+def extract_features(audio_data, sample_rate):
+    yt = nr.reduce_noise(y=audio_data, sr=sample_rate)
+    mfccs = librosa.feature.mfcc(y=yt, sr=sample_rate, n_mfcc=40)
+    mfccs_processed = np.mean(mfccs.T, axis=0)
+    return mfccs_processed
+
+# Streamlit app
+st.title("Bird Species Identification")
+
+# Create a file uploader component
+audio_file = st.file_uploader("Upload an audio file", type=["wav"])
+
+if audio_file:
+    st.audio(audio_file, format="audio/wav")
+
+    # Button to trigger prediction
+    if st.button("Identify Bird Species"):
+        st.write("Predicting bird species... Please wait.")
+        # Read the audio file and sample rate
+        audio_data, sample_rate = librosa.load(audio_file, sr=None)
+        le = LabelEncoder()
+        le.classes_ = np.array(['Bewick\'s Wren','Northern Mockingbird','American Robin','Song Sparrow','Northern Cardinal'])
+        predicted_species = predict_bird_species(audio_data, sample_rate, le)
+        st.success("Prediction complete!")
+
+        # Display the predicted species
+        st.subheader("Bird Species Prediction:")
+        st.write(f"The predicted bird species is: {predicted_species}")
