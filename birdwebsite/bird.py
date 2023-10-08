accf003c511ac8392411ccfa6cb7c0ebdad02278
Akhil0507 <85498044+AkhilNuthi@users.noreply.github.com>
2023-10-08 13:56:18 +0530

    Update bird.py

--- birdwebsite/bird.py
+++ birdwebsite/bird.py
@@ -23,6 +23,15 @@ def predict_bird_species(audio_data, sample_rate,le):
     predicted_class = le.inverse_transform([predicted_class_index])[0]
     return predicted_class
 
+# Define a dictionary mapping bird species to image URLs or file paths
+bird_images = {
+    'Bewick\'s Wren': 'birdwebsite/benwick.jpeg',
+    'Northern Mockingbird': 'birdwebsite/northmock.jpeg',
+    'American Robin': 'birdwebsite/americanrobin.jpeg',
+    'Song Sparrow': 'birdwebsite/songsparrow.jpeg',
+    'Northern Cardinal': 'birdwebsite/northcardinal.jpeg',
+
+
 def extract_features(audio_data, sample_rate):
     yt = nr.reduce_noise(y=audio_data, sr=sample_rate)
     mfccs = librosa.feature.mfcc(y=yt, sr=sample_rate, n_mfcc=40)
@@ -61,3 +70,12 @@ if audio_file:
         # Display the predicted species
         st.subheader("Bird Species Prediction:")
         st.write(f"The predicted bird species is: {predicted_species}")
+        # Display the corresponding bird image
+        if predicted_species in bird_images:
+            bird_image_path = bird_images[predicted_species]
+            st.image(bird_image_path, caption=f"{predicted_species} Image", use_column_width=True)
+        else:
+            st.warning("No image available for the predicted bird species.")
+
+
+
